6dd517160741c31ee2888b1a041dd294d682de38
romeovidal-yeayea <romeo.vidal@ucas-edu.net>
2025-08-29 14:42:39 -0600

    variables notes

---
+++ notes/variables.py
@@ -0,0 +1,19 @@
+# RV 7th variables Notes
+
+#What is one way to have your program give an output? by giving you information
+print("ayo wsg chat")
+
+#examples of variables
+school = "UCAS"
+single_digit = 4
+how_much = 10
+
+print(how_much)
+
+how_much = 200
+
+print(how_much)
+
+name = input("what is your name?")
+
+input("What is your name?")
