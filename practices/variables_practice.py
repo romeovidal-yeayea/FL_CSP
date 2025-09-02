b91012accf1a8a905653b5f68097c165e3f4025f
romeovidal-yeayea <romeo.vidal@ucas-edu.net>
2025-09-02 14:42:37 -0600

    added variables

--- practices/variables_practice.py
+++ practices/variables_practice.py
@@ -1,9 +1,24 @@
 #RV 7 period Variables practice
 
-name = Romeo
-number = 7
-Number = 667
-breakfast = eggs and beacon
-color = red 
-school = UCAS 
+name = "romeo"
+number = "7"
+Number = "667"
+breakfast = "eggs and beacon"
+color = "red" 
+school = "UCAS "
+year = "2025"
+eye = "brown"
+age = "14"
+subject = "science"
 
+
+print(name)
+print(number)
+print(Number)
+print(breakfast)
+print(color)
+print(school)
+print(year)
+print(eye)
+print(age)
+print(subject)
