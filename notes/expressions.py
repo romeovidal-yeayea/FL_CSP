7c5f80c03bab79a81e7b8ce1a006e991ca05da53
romeovidal-yeayea <romeo.vidal@ucas-edu.net>
2025-09-04 14:41:42 -0600

    took notes

---
+++ notes/expressions.py
@@ -0,0 +1,39 @@
+# RV 7th Expressions notes
+# intergers = whole numbers
+#flouts = numbers with decimal
+# What is an algorithm 
+    #just a set of instrucrtiobs to complete a task
+name = input("What is your name\n")
+print("Hello", name)
+
+#treyson has 12 apples, he has 5 friends that he wants to give apples to. How many apples does each friend get?
+apples = 12 
+friends = 5 
+print("Each friend gets", apples/friends, "apples")\
+
+# List steps in algorithms
+#1, declare the variables
+#2. do the equations/math
+#3. display results
+# Average age of a group of 4 people
+friends = 4
+age_1 = 12
+age_2 = 14
+age_3 = 14
+age_4 = 18
+
+total_ages = age_1 + age_2 + age_3 + age_4 
+average = total_ages/4
+print("The average age is", average)
+
+# List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+num_1 = 12
+num_2 = 7
+
+print("Addition(+): ", num_1+num_2)
+print("subtraction(-): ", num_1-num_2)
+print("multiplication(*): ", num_1*num_2)
+print("division(/): ", num_1/num_2)
+print("exponents(**): ", num_1**num_2)
+print("integer division(//): ", num_1//num_2)
+print("modulo(%): ", num_1%num_2)
