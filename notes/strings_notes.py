d7d7463a4078ea4eaaa5416f967c82268649b4dd
romeovidal-yeayea <romeo.vidal@ucas-edu.net>
2025-09-08 14:42:42 -0600

    added notes

--- notes/strings_notes.py
+++ notes/strings_notes.py
@@ -1,1 +1,16 @@
-#RV 7th strings
+#RV 7th strings Notes
+
+#1. What makes something a string?
+#a set of symbles that hold together by quotation marks
+
+#Examples
+first_name = input("What is your name:\n").strip().title()
+
+last_name = input("what is your last name:\n")
+
+full_name = first_name + " " + last_name
+sentence = 'The quick brown fox jumps over the lazy dog'
+print(sentence.strip())
+
+print("Welcome to my program", full_name + "!")
+#2. Why do we have strings?fer
