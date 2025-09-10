43613715729a3685f23efe399accc87fb0dcc0b9
romeovidal-yeayea <romeo.vidal@ucas-edu.net>
2025-09-10 14:11:06 -0600

    added calculator and first program

--- notes/strings_notes.py
+++ notes/strings_notes.py
@@ -6,12 +6,33 @@
 #Examples
 first_name = input("What is your name:\n").strip().title()
 
-last_name = input("what is your last name:\n")
+last_name = input("what is your last name:\n").strip().title()
 
 full_name = first_name + " " + last_name
-sentence = 'The quick brown fox jumps over the lazy dog'
-print(sentence.strip())
+sentence = 'The quick brown fox jumps over the lazy dog.'.strip()
+print(sentence.find("jumps"))
+print(sentence[20:25])
+print(sentence[sentence.find("lazy"): len("lazy")+sentence.find("lazy")])
 
 print("Welcome to my program", full_name + "!")
-#2. Why do we have strings?
+
+#sanitization and stupid proofing
+
+#stupid proofing prevents the user to break down coding
+
+#debuging is fixing code
+
+#debug and bugs
+#bug 1 = sintax error = a mistake in your code
+#error = "this is a bug"
+#how do you get them? ny mispelling, forget a variable and uppercases
+#bug 2 = logic error 
+#numOne = "1"
+#numTwo = "2"
+
+#print(numOne + numTwo)
  
+ #run-time error
+ 
+# num = 3
+#print("my favorite number is"+num)
