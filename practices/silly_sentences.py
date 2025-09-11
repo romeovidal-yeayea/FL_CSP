c389ca546670452f22777039ee7fa6ac32cfa08b
fersitov <romeo.vidal@ucas-edu.net>
2025-09-11 02:04:41 +0000

    finished silly sentences

---
+++ practices/silly_sentences.py
@@ -0,0 +1,42 @@
+# RV 7th silly_sentences
+
+color = input("Tell me a color: ")
+
+superhero = input("Tell me a superhero: ")
+
+place = input("Tell me a place: ")
+
+drink = input("Tell me a drink: ")
+
+verb = input("Tell me a verb: ")
+
+animal = input("Tell me a animal: ")
+
+num1 = input("Tell me num1: ")
+
+num2 = input("Tell me a num2: ")
+
+celebrity = input("Tell me a celebrity: ")
+
+sport = input("Tell me another vehicle: ")
+
+vehicle = input("Tell me a vehicle: ")
+
+animals2 = input("Tell me animals: ")
+
+car = input("Tell me a car: ")
+
+print("If I could design my own world, everything would be", color, "and ruled by", superhero + ".",
+
+      "Everyone would live in", place, "and eat nothing but", food, "with", drink, "to drink.",
+
+      "Instead of working, people would", verb, "with their pet", animal, "all day.",
+
+      "Money would be replaced with", object1 + ".",
+
+      "Every person would get", number1, "vacations a year and sleep", number2, "hours a night.",
+
+      "For entertainment, we’d watch", celebrity, "play", sport + ",",
+
+      "and travel everywhere in flying", vehicle + ".")
+
