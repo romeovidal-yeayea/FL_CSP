b42056bff802995b57a3ac6de4f2173caf56f812
romeovidal-yeayea <romeo.vidal@ucas-edu.net>
2025-09-18 14:39:33 -0600

    finished old enough

---
+++ practices/old_enough.py
@@ -0,0 +1,12 @@
+#RV 7th old enough
+
+age = int(input("How old are you\n"))
+
+if age >= 18:
+    print(f"You can start voting!")
+elif age >= 16:
+    print(f"You can start driving!")
+elif age >= 15:
+    print(f"You can go get your learners permit!")
+elif age >= 4:
+    print(f"You can go to school!")
