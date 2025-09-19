a751a40dcb5e6f47bd2f5a235c6828cbbce5d59c
romeovidal-yeayea <romeo.vidal@ucas-edu.net>
2025-09-19 14:03:49 -0600

    time notes

---
+++ notes/mytime.py
@@ -0,0 +1,16 @@
+#RV 7th notes about getting time
+
+import time
+import datetime
+
+epoch = time.time()
+readable_time = time.ctime(epoch)
+
+print(f"The time in seconds since Jan 1, 1970: {epoch}")
+print(f"The time is: {readable_time}")
+
+now = datetime.datetime.now()
+hour = now.hour
+
+print(f"The time is {now}")
+print(f"The hour is: {hour}")
