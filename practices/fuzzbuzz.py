b49575c3e48e7e158ab33e9d77d3d1ad669ea77f
romeovidal-yeayea <romeo.vidal@ucas-edu.net>
2025-09-23 14:16:01 -0600

    Fininshed assignment

---
+++ practices/fuzzbuzz.py
@@ -0,0 +1,15 @@
+#RV 7th fuzzbuzz
+
+x = 1
+
+while x <=50:
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 5 == 0:
+        print(("Buzz!"))
+    elif x % 3 == 0:
+        print("Fizz")
+    else:
+        print(x)
+    x+=1 
+print("Thats all numbers through 50")
