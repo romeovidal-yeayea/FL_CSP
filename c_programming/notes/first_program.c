a7e0cfe6ea7fadde31101103d140073df3576111
fersitov <romeo.vidal@ucas-edu.net>
2025-10-08 14:14:40 -0600

    Update first_program.c

---
+++ c_programming/notes/first_program.c
@@ -0,0 +1,13 @@
+#RV 7th Frist Program C
+#include <stdio.h>
+
+int main(){
+    char name[50];
+
+    printf("Please enter your name: ");
+    scanf("%49s", name)
+
+    printf("Hello, %s! Welcome to the first C program!\n", name)
+
+    return 0;
+}
