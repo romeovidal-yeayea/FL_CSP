735e0d7acdc41cc2fb0acd6ccf98065cbe463ae5
fersitov <romeo.vidal@ucas-edu.net>
2025-10-13 21:47:11 -0700

    Update name_decor.c

--- c_programming/practices/name_decor.c
+++ c_programming/practices/name_decor.c
@@ -1,12 +1,15 @@
-// Rv 7th Name decor
-
+// Rv 7th Name Decorator
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-    char name[50];
-    char character[10];
-    char finish_name[75];
+    int main(void){
+    char name[30];
+    char decorated_name[30] = "///)" ;
+    char name_decor[20] = "LLLL";
+   
+    printf("What{s your name\n");
+    scanf("%s", decorated_name, name);
+    strcat(decorated_name,name_decor);
+    printf("%s",decorated_name);
 
-    
 }
