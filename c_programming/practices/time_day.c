2993c2ad691ed36c021b01258ac2949d91910414
fersitov <romeo.vidal@ucas-edu.net>
2025-10-13 21:59:10 -0700

    Create time_day.c

---
+++ c_programming/practices/time_day.c
@@ -0,0 +1,21 @@
+// Rv 7th Time of day
+#include <stdio.h>
+#include <string.h>
+int main (void){
+    int time;
+    char name[50];
+    printf("Ayo, What time is it?:");
+    scanf("%d",&time);
+    if(time >= 13){
+   
+    printf("its afternoon;");
+     }
+    else if(time <= 10){
+     printf("it's still so early;
+     }
+     else if(time <= 12){
+     printf("it is evening");
+     }
+   
+   
+   
