1658f9065c63e4d2e7193333ab984c5db7bab97d
romeovidal-yeayea <romeo.vidal@ucas-edu.net>
2025-10-15 14:35:22 -0600

    hello world

---
+++ c_programming/practices/update_calc.c
@@ -0,0 +1,18 @@
+// RV 7th Financial calculators
+#include <stdio.h>
+
+int main(void) {
+     float income, rent, utilities, groceries, transport, savings, remaining;
+     float 
+
+
+
+
+
+
+
+
+
+
+     
+}
---
+++ c_programming/practices/update_helloworld.c
@@ -0,0 +1,14 @@
+// 
+# include <stdio.h>
+
+
+void name_finder(char* name_of_person){
+    printf("Hey, %s. Ur name fire\n", name_of_person);
+}
+int main(void){
+    name_finder("Jake");
+    name_finder("Lindon");
+    name_finder("Ciara");
+    name_finder("Daniel");
+    name_finder("Jesus");
+}
